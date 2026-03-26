2c7f6553e9f0e1b089b6e7a91bf8f1815b66d89a
JoshiCodes <55353244+JoshiCodes@users.noreply.github.com>
2026-03-26 20:04:44 +0100

    feat: add frontend URL configuration to environment and application properties

---
+++ backend/src/main/java/de/joshicodes/aren/resource/internal/OAuthResources.java
@@ -0,0 +1,39 @@
+package de.joshicodes.aren.resource.internal;
+
+import io.quarkus.security.Authenticated;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Path("/api/internal/projects")
+@Authenticated
+public class OAuthResources {
+    
+    @ConfigProperty(name = "aren.frontend.url")
+    String frontendUrl;
+
+    /**
+     * The default endpoint redirected to by the client.
+     * Checks for GET/PATH params:
+     *  -
+     * Creates a data object,
+     * redirects to frontend with /consent?req=[data obj. id]
+     * @return a redirect response
+     */
+    @GET
+    public Response authorize() {
+        try {
+            return Response.seeOther(
+                    new URI("")
+            ).build();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return Response.status(500).build();
+        }
+    }
+
+}

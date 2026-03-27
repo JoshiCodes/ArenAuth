3bfdf10890f95bd3c83459e59fe2d1f6345aa817
JoshiCodes <55353244+JoshiCodes@users.noreply.github.com>
2026-03-27 18:32:55 +0100

    feat: update BarInput component to allow nullable label and adjust button positioning

--- frontend/src/lib/components/ui/BarInput.svelte
+++ frontend/src/lib/components/ui/BarInput.svelte
@@ -5,7 +5,7 @@
 
     export let type: HTMLInputTypeAttribute = "text";
     export let id: string;
-    export let label: string;
+    export let label: string|null;
     export let placeholder: string = "";
 
     export let disabled: boolean = false;
@@ -34,12 +34,12 @@
                class="border-box w-full
                bg-zinc-200 dark:bg-zinc-800
                rounded-lg
-               py-2 px-4
+               py-2 px-4 mt-2
                disabled:cursor-not-allowed
         " disabled={disabled} placeholder={placeholder} />
         {#if copyButton}
             <button title="Copy" type="button"
-                    class="absolute right-2 top-[40%] z-1
+                    class="absolute right-2 {label ? 'top-[48%]' : 'top-[22%]'} z-1
                      p-1 md:p-2 rounded-lg
                     {
                     copied ?
@@ -52,15 +52,15 @@
             " onclick={copy}
                 disabled={copyButtonDisabled}
             >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                     {#if copied}
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
                         </svg>
                     {:else}
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
+                        </svg>
                     {/if}
-                </svg>
             </button>
         {/if}
     </div>

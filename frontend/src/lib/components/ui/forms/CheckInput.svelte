8f8a6736032337c0be51d747eb86748a9f34a3b4
JoshiCodes <55353244+JoshiCodes@users.noreply.github.com>
2026-03-28 11:27:37 +0100

    feat: enhance checkbox input with disabled and required attributes

--- frontend/src/lib/components/ui/forms/CheckInput.svelte
+++ frontend/src/lib/components/ui/forms/CheckInput.svelte
@@ -13,6 +13,6 @@
 </script>
 
 <div class="flex content-center items-center my-2">
-    <input id={id} type="checkbox" bind:checked={checked} on:change={onChange} class="w-4 h-4 border border-default-medium rounded-xl accent-violet-500 dark:accent-violet-600 bg-neutral-secondary-medium focus:ring-violet-500 dark:focus:ring-violet-600">
+    <input id={id} type="checkbox" bind:checked={checked} on:change={onChange} disabled={disabled} required={required} class="w-4 h-4 border border-default-medium rounded-xl accent-violet-500 dark:accent-violet-600 bg-neutral-secondary-medium focus:ring-violet-500 dark:focus:ring-violet-600">
     <label for={id} class="select-none ms-2 font-medium text-heading">{label}</label>
 </div>

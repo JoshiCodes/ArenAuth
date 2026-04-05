c5dd187a973c7b54a469d74330d5e75b2bbf2f7b
JoshiCodes <55353244+JoshiCodes@users.noreply.github.com>
2026-04-06 00:59:50 +0200

    fix: add debug logging for session cookie authentication

--- backend/src/main/java/de/joshicodes/aren/security/SessionCookieAuthMechanism.java
+++ backend/src/main/java/de/joshicodes/aren/security/SessionCookieAuthMechanism.java
@@ -8,12 +8,14 @@ import io.quarkus.vertx.http.runtime.security.HttpAuthenticationMechanism;
 import io.smallrye.mutiny.Uni;
 import io.vertx.core.http.Cookie;
 import io.vertx.ext.web.RoutingContext;
+import jakarta.annotation.Priority;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
 import java.security.Principal;
 
 @ApplicationScoped
+@Priority(10)
 public class SessionCookieAuthMechanism implements HttpAuthenticationMechanism {
 
     public static final String COOKIE_NAME = "auth_session";
@@ -23,9 +25,11 @@ public class SessionCookieAuthMechanism implements HttpAuthenticationMechanism {
     @Override
     public Uni<SecurityIdentity> authenticate(RoutingContext context, IdentityProviderManager identityProviderManager) {
         final Cookie cookie = context.request().getCookie(COOKIE_NAME);
+        System.out.println("Auth Cookie: " + cookie);
         if(cookie == null || cookie.getValue() == null || cookie.getValue().isBlank()) {
             return Uni.createFrom().nullItem();
         }
+        System.out.println(cookie.getName() + " = " + cookie.getValue());
 
         return Uni.createFrom().optional(() -> sessionService.getSession(cookie.getValue()))
                 .onItem().ifNotNull().transform(session -> {
